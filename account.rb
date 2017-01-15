ed2f30ea7e8695845ab20404bb5bce291e0d445f
Takashi Osako <tosako@handy.com>
2017-01-15 14:24:05 -0500

    extra step, removed the method in the source class

--- account.rb
+++ account.rb
@@ -1,13 +1,11 @@
 class Account
   BANK_FEE = 4.5
 
-  def overdraft_change
-    @account_type.overdraft_change(@days_overdrawn)
-  end
-
   def bacnk_charge
     result = BANK_FEE
-    result += overdraft_change if @days_overdrawn > 0
+    if @days_overdrawn > 0
+      result += @account_type.overdraft_change(@days_overdrawn)
+    end
     result
   end
 
