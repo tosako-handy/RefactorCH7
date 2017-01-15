fce1009d3ac1b8bbec80c6f6571c6bc4ff007201
Takashi Osako <tosako@handy.com>
2017-01-15 18:08:46 -0500

    link from the person to the telephone number

--- person.rb
+++ person.rb
@@ -4,6 +4,10 @@ class Person
   attr_accessor :office_area_code
   attr_accessor :office_number
 
+  def initialize
+    @office_telephone = TelephoneNumber.new
+  end
+
   def telephone_number
     "(#{office_area_code}) #{office_number}"
   end
