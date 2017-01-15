0f86b1659b1614b5a274d01a249c2f3ce764a397
Takashi Osako <tosako@handy.com>
2017-01-15 14:16:36 -0500

    copy overdraft_charge method from Account to AccountType

--- account_type.rb
+++ account_type.rb
@@ -1,3 +1,16 @@
 class AccountType
+  PREMIUM_DEFAULT_CHARGE = 10
+  PREMIUM_GRACE_PERIOD = 7
+  PREMIUM_MULTIPLIER = 0.85
+  REGULAR_MULTIPLIER = 4.5
+  def overdraft_charge(days_overdrawn)
+    if premium?
+      result = PREMIUM_INITIAL_CHARGE
+      result += (days_overdrawn - PREMIUM_GRACE_PERIOD) * PREMIUM_MULTIPLIER if days_overdrawn > PREMIUM_GRACE_PERIOD
+      result
+    else
+      days_overdrawn * REGULAR_MULTIPLIER
+    end
+  end
 end
 
