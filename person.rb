9eaebe14be9e4f0c5fb9c6018a784b6588744d08
Takashi Osako <tosako@handy.com>
2017-01-15 18:15:13 -0500

    move the other field and use Move Method on the telephone number

--- person.rb
+++ person.rb
@@ -1,22 +1,17 @@
 class Person
 
   attr_reader :name
-  attr_accessor :office_number
 
   def initialize
     @office_telephone = TelephoneNumber.new
   end
 
   def telephone_number
-    "(#{office_area_code}) #{office_number}"
+    @office_telephone.telephone_number
   end
 
-  def office_area_code
-    @office_telephone.area_code
-  end
-
-  def office_area_code=(arg)
-    @office_telephone.area_code = arg
+  def office_telephone
+    @offce_telephone
   end
 end
 
--- telephone_number.rb
+++ telephone_number.rb
@@ -1,4 +1,9 @@
 class TelephoneNumber
   attr_accessor :area_code
+  attr_accessor :number
+
+  def telephone_number
+    "(#{areacode}) #{number}"
+  end
 end
 
