23bdd0b87de407023129b02ebc63eb06e40991c5
Takashi Osako <tosako@handy.com>
2017-01-15 14:18:25 -0500

    replace the source method body with delegation run test make sure it passes

--- account.rb
+++ account.rb
@@ -1,18 +1,8 @@
 class Account
-  PREMIUM_DEFAULT_CHARGE = 10
-  PREMIUM_GRACE_PERIOD = 7
-  PREMIUM_MULTIPLIER = 0.85
-  REGULAR_MULTIPLIER = 1.75
   BANK_FEE = 4.5
 
   def overdraft_change
-    if @account_type.premium?
-      result = PREMIUM_INITIAL_CHARGE
-      result += (@days_overdrawn - PREMIUM_GRACE_PERIOD) * PREMIUM_MULTIPLIER if @days_overdrawn > PREMIUM_GRACE_PERIOD
-      result
-    else
-      @days_overdrawn * REGULAR_MULTIPLIER
-    end
+    @account_type.overdraft_change(@days_overdrawn)
   end
 
   def bacnk_charge
